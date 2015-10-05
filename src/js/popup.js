b9440d9e311a4f98123504340477f0202b9034df
Dmitry Parshin <gasperoid@yandex.ru>
2015-10-05 17:22:04 +0300

    What done is done. Enough for today.

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,6 +1,31 @@
+// Semicolon (;) to ensure closing of earlier scripting
+// Encapsulation
+// $ is assigned to jQuery
+;(function($) {
+
+     // DOM Ready
+    $(function() {
+
+        // Binding a click event
+        // From jQuery v.1.7.0 use .on() instead of .bind()
+        $('#my-button').bind('click', function(e) {
+
+            // Prevents the default action to be triggered. 
+            e.preventDefault();
+
+            // Triggering bPopup when click event is fired
+            $('#element_to_pop_up').bPopup();
+
+        });
+
+    });
+
+})(jQuery);
+
+
 $('element_to_pop_up').bPopup({
     modalClose: false,
     opacity: 0.6,
-    positionStyle: 'fixed' //'fixed' or 'absolute'
+    // positionStyle: 'fixed' //'fixed' or 'absolute'
 });
             
